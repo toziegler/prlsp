09299a62d27a139a70e900e848f366b8f94285ea
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2026-03-05 14:16:13 +0100

    Build flake for nixos config.

--- flake.nix
+++ flake.nix
@@ -7,16 +7,37 @@
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        inherit system;
         pkgs = nixpkgs.legacyPackages.${system};
       });
     in {
-      devShells = forAllSystems ({ pkgs }: {
+      packages = forAllSystems ({ pkgs, ... }:
+        let
+          prlsp-go = pkgs.buildGoModule {
+            pname = "prlsp-go";
+            version = "0.1.0";
+            src = ./go;
+            vendorHash = null;
+          };
+        in {
+          default = prlsp-go;
+          prlsp-go = prlsp-go;
+        });
+
+      apps = forAllSystems ({ pkgs, ... }: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.system}.default}/bin/prlsp-go";
+        };
+      });
+
+      devShells = forAllSystems ({ pkgs, ... }: {
         default = pkgs.mkShell {
           packages = [
             pkgs.go
-            pkgs.neovim
             pkgs.gh
             pkgs.git
+            self.packages.${pkgs.system}.default
           ];
         };
       });
