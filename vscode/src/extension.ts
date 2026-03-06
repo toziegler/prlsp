eaa02b6e279536ff540ee1d077e999703ec95373
Alex Kladov <aleksey.kladov@gmail.com>
2026-03-06 00:09:37 +0000

    start vscode extension

---
+++ vscode/src/extension.ts
@@ -0,0 +1,32 @@
+import * as path from "path";
+import { ExtensionContext } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient | undefined;
+
+export function activate(context: ExtensionContext) {
+  const goDir = path.resolve(context.extensionPath, "..", "go");
+
+  const serverOptions: ServerOptions = {
+    command: "go",
+    args: ["run", "."],
+    options: { cwd: goDir },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", pattern: "**/*" }],
+  };
+
+  client = new LanguageClient("prlsp", "prlsp", serverOptions, clientOptions);
+  client.start();
+}
+
+export async function deactivate(): Promise<void> {
+  if (client) {
+    await client.stop();
+  }
+}
